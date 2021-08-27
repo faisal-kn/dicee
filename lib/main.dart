3f1a3d78e7fb190ec83cd4f63383274e6528d765
faisal-kn <khanmohdfaisal75@gmail.com>
2021-08-27 19:48:12 +0530

    initialized Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void Main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
