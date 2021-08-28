331518817d93aabf4cd9366ff4a88e27102fe752
faisal-kn <khanmohdfaisal75@gmail.com>
2021-08-28 19:01:45 +0530

    Ui created

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 
-void Main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-  
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(
+            child: Text(
+              'Dicee',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+            ),
+          ),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  'Let\'s play dicee',
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w300),
+                ),
+              ),
+              SizedBox(
+                width:130.0,
+                height: 25.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Divider(
+                    height: 5.0,
+                    thickness:5.0,
+                    color:Colors.white70,
+                  ),
+                ),
+              ),
+              Row(children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                      onPressed: () {
+                        print('Button got pressed');
+                      },
+                      child: Image.asset('images/dice1.png')),
+                ),
+                Expanded(
+                  child: FlatButton(
+                      onPressed: () {
+                        print('Button 2 got pressed');
+                      },
+                      child: Image.asset('images/dice2.png')),
+                ),
+              ])
+            ]),
+      ),
+    );
   }
 }
